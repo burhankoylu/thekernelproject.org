43d5e3a4ada40c1c177ea45aea3a2bee1ad37e68
Burhan Köylü <burhankoylu@outlook.com>
2023-11-28 00:25:37 +0300

    add languages

--- contact/script.js
+++ contact/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     var menuButton = document.getElementById('menuButton');
+    var languageButton = document.getElementById('languageButton');
     var aside = document.querySelector('aside');
     var isMenuOpen = false;
 
-    menuButton.addEventListener('click', function () {
+    menuButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
         if (!isMenuOpen) {
             aside.style.width = '200px';
             isMenuOpen = true;
@@ -11,5 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
             aside.style.width = '0';
             isMenuOpen = false;
         }
+    });
+
+    languageButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var languageMenu = document.querySelector('.language-menu');
+        languageMenu.classList.toggle('show-languages');
     });
 });
